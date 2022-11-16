f081a69baff7edbca6153d681b81e3a921aba13e
Aishwarya Khandelwal <aishwarya.khandelwal@idctechnologies.com>
2022-11-16 10:59:41 +0530

    file3.js added

---
+++ file3.js
@@ -0,0 +1,10 @@
+// let person = {
+//     firstname: 'aishwarya',
+//     lastname: 'k',
+//     getFunction: function () {
+//         return (`name of the person ${person.firstname} ${person.lastname}');
+
+//     }
+// }
+// console.log(person.getFunction);
+console.log("creating simple repo");
